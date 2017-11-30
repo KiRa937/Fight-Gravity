56126a820c246ece7f5206d016bcf9a79752dcf5
KiRa937 <KiRa937@users.noreply.github.com>
2017-11-30 15:52:47 +0300

    Sound sliders

---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    public Slider musicVolumeSlider;
+    public Slider soundVolumeSlider;
+
+	public AudioMixer mixer;
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+
+		musicVolumeSlider.onValueChanged.AddListener(delegate {OnMusicVolumeChange();});
+		soundVolumeSlider.onValueChanged.AddListener(delegate {OnSoundVolumeChange();});
+
+    }
+
+    public void OnMusicVolumeChange()
+    {
+		//musicSource.volume = musicVolumeSlider.value;
+		mixer.SetFloat("musicVol", musicVolumeSlider.value);
+    }
+
+    public void OnSoundVolumeChange()
+    {
+		mixer.SetFloat("sfxVol", soundVolumeSlider.value);
+    }
+}
